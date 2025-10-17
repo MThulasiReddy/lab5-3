24cb41ea5ce5d087548813a9e303aebc1b3484cd
Thulasi <thulasireddy099@gmail.com>
2025-10-17 21:58:25 +0530

    project v1

---
+++ src/main/java/com/example/Lab53Application.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class Lab53Application extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab53Application.class, args);
+		System.out.println("project is running....");
+	}
+
+}
